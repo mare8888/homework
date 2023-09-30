549baf370d223653aa548399429e875d6babcbdd
mare8888 <maretskij.a@gmail.com>
2023-09-30 22:35:15 +0300

    zdelano

--- src/MyhomeWork1.java
+++ src/MyhomeWork1.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class MyhomeWork1 {
+public class MyhomeWork1 {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 100;
+        int i = 1000;
+        long l = 10000;
+        float f = 2.5f;
+        double d = 3.7;
+        boolean bo = true;
+        char ch = '\uFFFF';
+        System.out.println(b + " " + s + " " + i + " " + l + " " + f + " " + d + " " + bo + " " + ch);
+
+        int sum = s + i;
+        System.out.println(sum);
+
+        int sun2 = b + s;
+        System.out.println(sun2);
+        vvv();
+    }
+    public static void vvv(){
+        int z = 35;
+        long w = 65;
+        long sum1 = z + w;
+        System.out.println(sum1);
+    }
 }
+
+
