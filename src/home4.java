d7b0fff37f691ba28bcc49b311eb0615270d8c65
mare8888 <maretskij.a@gmail.com>
2023-10-06 00:49:07 +0200

    fisrt3

--- src/home4.java
+++ src/home4.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class home4 {
-}
+import java.util.Scanner;
+
+public class home4 {
+        public static void main(String[] args) {
+    /*  ЗАДАНИЕ 1 для запуска раскоментировать
+        Scanner input = new Scanner(System.in);
+            System.out.println("Enter array length: ");
+            int size = input.nextInt();
+            int array[] = new int[size];
+            System.out.println("Insert array elements:");
+
+            for (int i = 0; i < size; i++) {
+                array[i] = input.nextInt();
+            }
+            System.out.print ("Inserted array elements:");
+            for (int i = 0; i < size; i++) {
+                System.out.print (" " + array[i]);
+            }
+            System.out.println();
+*/
+/*    ЗАДАНИЕ 2 для запуска раскоментировать
+ for (int i = 9; i >= 0; i--){
+                for (int j = 0; j < 10; j++){
+                    System.out.println(i + " * " + j + " * " + i*j);
+                }
+                System.out.println();
+            } */
+
+ /*     ЗАДАНИЕ 3 для запуска раскоментировать
+    Scanner scanner = new Scanner(System.in);
+
+
+            System.out.println("Введите число: " );
+            int a = scanner.nextInt();
+             while(a >= 0){
+                 System.out.println("Ваше число равно = " + a);
+                 break;
+             }
+*/
+ /*   ЗАДАНИЕ 3.1 для запуска раскоментировать
+   Scanner scanner = new Scanner(System.in);
+
+            do {
+                System.out.println("Какая сегодня была температура ? Введите число: ");
+
+            } while(scanner.nextInt() == 5);
+            */
+
+            int[][] array = new int[4][8];
+
+
+              }
+
+        }
